0d86b70e5b5837cd9a3c8fdc02f53b30609882ec
Mario Ceresa <mario.ceresa@gmail.com>
2019-07-28 23:17:57 +0200

    Updated building pipeline

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,50 +1,71 @@
+using System.IO;
 using UnityEditor;
+using System.Diagnostics;
+
 class Builder {
+    [MenuItem("CustomScripts/Build Linux")]
     static void buildLinux() {
 
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        UnityEditor.BuildPlayerOptions buildPlayerOptions = new UnityEditor.BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"
                           };
-        buildPlayerOptions.locationPathName = "builds/clientLinux";
+        string dir = "builds/linuxClient";
+        
+        Directory.CreateDirectory(dir);
+        
+        buildPlayerOptions.locationPathName = dir + "/clientLinux.x86_64";
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.options = BuildOptions.None;
 
         BuildPipeline.BuildPlayer(buildPlayerOptions);        
      
     }
-    
+    [MenuItem("CustomScripts/Build Server")]    
     static void buildHeadless() {
 
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"
                           };
-        buildPlayerOptions.locationPathName = "builds/serverHeadless";
+        string dir = "builds/linuxServer";
+        
+        Directory.CreateDirectory(dir);
+                                  
+        buildPlayerOptions.locationPathName = dir + "/headless.x86_64";
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.options = BuildOptions.EnableHeadlessMode;
 
         BuildPipeline.BuildPlayer(buildPlayerOptions);        
      
     }
-    
+    [MenuItem("CustomScripts/Build Mac")]    
     static void buildMac() {
-
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"
                           };
-        buildPlayerOptions.locationPathName = "builds/clientMac";
+                          
+        string dir = "builds/macClient";
+        
+        Directory.CreateDirectory(dir);
+                                  
+        buildPlayerOptions.locationPathName = dir + "/clientMac";
         buildPlayerOptions.target = BuildTarget.StandaloneOSX;
         buildPlayerOptions.options = BuildOptions.None;
 
         BuildPipeline.BuildPlayer(buildPlayerOptions);        
      
     }    
-    
+
+    [MenuItem("CustomScripts/Build Win64")]    
     static void buildWin64() {
 
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = new[] {"Assets/Scenes/SampleScene.unity"
                           };
-        buildPlayerOptions.locationPathName = "builds/clientWin64";
+        string dir = "builds/win64Client";
+        
+        Directory.CreateDirectory(dir);
+                                  
+        buildPlayerOptions.locationPathName = dir + "/clientWin64.exe";
         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
         buildPlayerOptions.options = BuildOptions.None;
 
--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 export TERM=xterm
 ppath=$(pwd)
+logfile=$ppath"/buildLinux.log"
 unitybin="$HOME/Unity/Hub/Editor/2018.3.14f1/Editor/Unity"
-$unitybin -quit -batchmode -logFile buildLinux.log -projectPath $ppath -executeMethod Builder.buildLinux
+set -x
+$unitybin -quit -batchmode -logFile $logfile -projectPath $ppath -executeMethod Builder.buildLinux &
+rm $logfile && touch $logfile && tail -f $logfile
